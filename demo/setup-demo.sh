a8a057f092e882053bd801ca27ca159f492ccc0c
Le Hoang Long <le.hoang.long@outlook.com>
2025-03-10 18:59:27 +0700

    update

--- demo/setup-demo.sh
+++ demo/setup-demo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd /home/frappe/erpnext/erpnext
 source /home/frappe/erpnext/venv/bin/activate
 source /home/frappe/erpnext/new-site.sh producer
 source /home/frappe/erpnext/new-site.sh distributor
