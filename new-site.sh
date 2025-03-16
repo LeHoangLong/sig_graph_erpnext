45898d3b3ebf99bd73971730434fe2c066d423f4
Le Hoang Long <le.hoang.long@outlook.com>
2025-03-16 16:57:07 +0700

    update

--- new-site.sh
+++ new-site.sh
@@ -5,5 +5,6 @@ then
   source venv/bin/activate
 fi
 
+cd erpnext
 bench new-site --mariadb-user-host-login-scope % --db-host db --db-name $1 --db-root-username root --db-password password $1
 bench --site $1 install-app erpnext
