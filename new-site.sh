25b92d623f3c4677bd2309d14f82052f81983166
Le Hoang Long <le.hoang.long@outlook.com>
2025-03-16 16:52:52 +0700

    update

--- new-site.sh
+++ new-site.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+if [[ "$VIRTUAL_ENV" == "" ]]
+then
+  source venv/bin/activate
+fi
+
 bench new-site --mariadb-user-host-login-scope % --db-host db --db-name $1 --db-root-username root --db-password password $1
 bench --site $1 install-app erpnext
