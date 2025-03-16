805a885f24a27d47b9371d03035f644b0ef575c0
Le Hoang Long <le.hoang.long@outlook.com>
2025-03-16 17:09:49 +0700

    update

--- new-site.sh
+++ new-site.sh
@@ -6,5 +6,11 @@ then
 fi
 
 cd erpnext
+
+if [ ! -f ./sites/apps.txt ]; then
+  echo "frappe" > ./sites/apps.txt
+fi
+
+
 bench new-site --mariadb-user-host-login-scope % --db-host db --db-name $1 --db-root-username root --db-password password $1
 bench --site $1 install-app erpnext
